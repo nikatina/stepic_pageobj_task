39f79c96b8057164ff65215a1263b6f8147ff8b0
Nika Romane <nikatina93@yandex.ru>
2020-04-05 23:22:40 +0300

    test_guest_can_go_to_login_page_from_product_page added

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 import time
 
@@ -27,7 +28,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.compare_book_titles()
     page.compare_prices()
 
-
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -41,7 +42,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
-
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -49,6 +50,20 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.send_answer_and_accept_prompt()
     page.success_message_is_disappeared()
 
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 
